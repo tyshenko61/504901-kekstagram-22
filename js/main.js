6158fa180e3633f75a04739f501189403bc09d71
tyshenko61 <34244584+tyshenko61@users.noreply.github.com>
2021-01-31 12:35:24 +0500

    Утилитарные функции

---
+++ js/main.js
@@ -0,0 +1,33 @@
+'use strict';
+/**
+ * Returns a random positive integer between min (inclusive) and max (inclusive).
+ * The value is no lower than min (or the next integer greater than min
+ * if min isn't an integer) and no greater than max (or the next integer
+ * lower than max if max isn't an integer).
+ * Using Math.round() will give you a non-uniform distribution!
+ */
+
+const getRandomInt = function (min, max) {
+  if (min < 0) {
+    min = 0;
+  }
+  if (max < 0) {
+    max = 0;
+  }
+  if (min > max) {
+    let temp = min;
+    min = max;
+    max = temp;
+  }
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+/**
+ * checking the maximum string length
+*/
+const checkMaxLength = function (string, maxLength) {
+  return string.length <= maxLength;
+}
+
